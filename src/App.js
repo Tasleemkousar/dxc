e41a1150851a7b278c3dd627e3d9eeff1d137fff
Shaik Tasleem kousar <shaiktasleem989@gmail.com>
2021-11-09 22:56:16 -0800

    css modules

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './components/Greet';
+import Welcome from './components/Welcome';
+import Message from './components/Message';
+import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
+import Table from './components/Table';
+import Stylesheet from './components/Stylesheet';
+import Inline from './components/Inline';
+import './appStyles.css'
+import styles from './appStyles.module.css'
+ 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1 className = {styles.success}>success</h1>
+    <h1 className = 'error'>error</h1>
+
+    <Inline/>
+    <Stylesheet  enabled = {true}/>
+    <Table/>
+    <NameList/>
+    <UserGreeting/>
+
+    <ParentComponent/>
+    <EventBind/>
+    
     </div>
   );
 }
