c33ccbabb3e6f15c1f5ebe91fab1f5ef6c8b8b72
Shaik Tasleem kousar <shaiktasleem989@gmail.com>
2021-11-15 18:14:07 +0530

    hjkjl

--- src/App.js
+++ src/App.js
@@ -1,40 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
-import Welcome from './components/Welcome';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Table from './components/Table';
-import Stylesheet from './components/Stylesheet';
-import Inline from './components/Inline';
-import './appStyles.css'
-import styles from './appStyles.module.css'
-import Form from './components/Form';
+import FrParentInput from './components/refs/FrParentInput';
+
 
 
 function App() {
   return (
     <div className="App">
-    <Form/>
-
-      <h1 className={styles.success}>success</h1>
-      <h1 className='error'>error</h1>
-
-      <Inline />
-      <Stylesheet enabled={true} />
-      <Table />
-      <NameList />
-      <UserGreeting />
-
-      <ParentComponent />
-      <EventBind />
-
+    <FrParentInput/>
     </div>
   );
 }
